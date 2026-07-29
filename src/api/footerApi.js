d24dc59adea955f9a3412cc2068e851103669784
xusniddinovk1 <kxprogrammer1@gmail.com>
2026-07-29 21:33:17 +0500

    intiial commit

--- src/api/footerApi.js
+++ src/api/footerApi.js
@@ -1,10 +1,8 @@
 import api from "./axios";
 
-// DummyJSON'dan kategoriyalarni olish
 export const getFooterCategories = async () => {
   try {
     const res = await api.get("/products/categories");
-    // Birinchi 6 tasini kesib olamiz
     return res.data.slice(0, 6);
   } catch (error) {
     console.error("Kategoriyalarni olishda xatolik:", error);
